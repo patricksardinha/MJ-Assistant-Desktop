c0087953d5cb76686b4769339667e7a7436e4950
Patrick Sardinha <patricksardinha.pro@gmail.com>
2025-05-11 23:45:55 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <main className="container">
+      <h1 className="text-3xl font-bold text-shadow-slate-600">Welcome to Tauri + React</h1>
+    </main>
+  );
+}
+
+export default App;
